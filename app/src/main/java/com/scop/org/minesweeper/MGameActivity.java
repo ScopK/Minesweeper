f7b5a2fd84f6b0bbc10cc5cfa16555af147505ea
Scop <osc.car2@gmail.com>
2015-11-25 22:59:08 +0100

    Hello project

---
+++ app/src/main/java/com/scop/org/minesweeper/MGameActivity.java
@@ -0,0 +1,23 @@
+package com.scop.org.minesweeper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class MGameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(new GamePanel(this));
+    }
+}
